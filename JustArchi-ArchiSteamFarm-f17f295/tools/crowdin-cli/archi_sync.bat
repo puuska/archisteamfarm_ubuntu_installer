f0d905a4fedb61ba28ba61d41a6079e3eb94c3eb
Puuska <puszekkk@outlook.com>
2017-07-15 03:10:56 +0200

    removed test directory

--- JustArchi-ArchiSteamFarm-f17f295/tools/crowdin-cli/archi_sync.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd ..\\..
-call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml upload sources
-call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml download
-pause
